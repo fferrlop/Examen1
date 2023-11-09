1b054aeedbb4078a7f5b3d0f2767145793e3370f
fferrlop <fferrlop@myuax.com>
2023-11-09 11:43:40 +0100

    commit suma

--- main.cpp
+++ main.cpp
@@ -6,5 +6,17 @@ int numerosEnteros (int numero1, int numero2){
 }
 
 int main() {
+    int numero1, numero2;
 
+        std::cout << "Ingresa numero 1: ";
+        std::cin >> numero1;
+
+        std::cout << "Ingresa numero 2: ";
+        std::cin >> numero2;
+
+        int numeros = numerosEnteros(numero1, numero2);
+
+        std::cout << "la suma de los dos numeros enteros " << numero1 << "+" << numero2 << " = " << numeros << std::endl;
+
+    return 0;
 }
